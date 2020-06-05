a7b5f0baf582318016286eda867742c353cd154a
chuanJia <jiashiying971@outlook.com>
2020-06-05 20:57:04 +0800

    rate app v0.2 Config

--- app/src/main/java/com/example/jia_app/RateActivity.java
+++ app/src/main/java/com/example/jia_app/RateActivity.java
@@ -2,6 +2,7 @@ package com.example.jia_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -46,5 +47,34 @@ public class RateActivity extends AppCompatActivity {
         }else{
             show.setText(String.valueOf(r*wonRate));
         }
+    }
+    public void openOne(View btn){
+        Intent config = new Intent(this,ConfigActivity.class);
+        config.putExtra("dollar_rate_key",dollarRate);
+        config.putExtra("euro_rate_key",euroRate);
+        config.putExtra("won_rate_key",wonRate);
+
+        Log.i(TAG, "openOne: dollarRate=" + dollarRate);
+        Log.i(TAG, "openOne: euroRate=" + euroRate);
+        Log.i(TAG, "openOne: wonRate=" + wonRate);
+
+        //startActivity(config);
+        startActivityForResult(config,1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==1 && resultCode==2){
+
+            Bundle bundle = data.getExtras();
+            dollarRate = bundle.getFloat("key_dollar",0.1f);
+            euroRate = bundle.getFloat("key_euro",0.1f);
+            wonRate = bundle.getFloat("key_won",0.1f);
+            Log.i(TAG, "onActivityResult: dollarRate=" + dollarRate);
+            Log.i(TAG, "onActivityResult: euroRate=" + euroRate);
+            Log.i(TAG, "onActivityResult: wonRate=" + wonRate);
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
