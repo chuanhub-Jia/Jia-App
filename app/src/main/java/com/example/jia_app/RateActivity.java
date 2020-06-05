9566b874f127a6568f47c9b52053d0fb6444a797
chuanJia <jiashiying971@outlook.com>
2020-06-05 21:27:22 +0800

    rate app v0.4 obtain

--- app/src/main/java/com/example/jia_app/RateActivity.java
+++ app/src/main/java/com/example/jia_app/RateActivity.java
@@ -1,28 +1,54 @@
 package com.example.jia_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class RateActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class RateActivity extends AppCompatActivity implements Runnable{
     private final String TAG = "Rate";
     private float dollarRate = 0.142f;
     private float euroRate = 0.125f;
     private float wonRate = 0.3f;
     EditText rmb;
     TextView show;
+    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        Thread thread = new Thread(this);
+        thread.start();
+        handler =new Handler(){
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+
+                if(msg.what==5){
+                    String str = (String) msg.obj;
+                    Log.i(TAG, "handleMessage: getMessage msg = " + str);
+                    show.setText(str);
+                }
+                super.handleMessage(msg);
+            }
+        };
         SharedPreferences sharedPreferences = getSharedPreferences("myrate", Activity.MODE_PRIVATE);
 
         dollarRate = sharedPreferences.getFloat("dollar_rate",0.0f);
@@ -98,4 +124,51 @@ public class RateActivity extends AppCompatActivity {
 
         super.onActivityResult(requestCode, resultCode, data);
     }
+
+    @Override
+    public void run() {
+        Log.i(TAG, "run: run()......");
+        for(int i=1;i<3;i++){
+            Log.i(TAG, "run: i=" + i);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        URL url = null;
+        try {
+            url = new URL("http://www.usd-cny.com/icbc.htm");
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            InputStream in = http.getInputStream();
+
+            String html = inputStream2String(in);
+            Log.i(TAG, "run: html=" + html);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Message msg = handler.obtainMessage(5);
+        msg.obj = "Hello from run()";
+        handler.sendMessage(msg);
+    }
+
+    private String inputStream2String(InputStream inputStream) throws IOException {
+        final int bufferSize = 1024;
+        final char[] buffer = new char[bufferSize];
+        final StringBuilder out = new StringBuilder();
+        Reader in = new InputStreamReader(inputStream, "gb2312");
+        while (true) {
+            int rsz = in.read(buffer, 0, buffer.length);
+            if (rsz < 0)
+                break;
+            out.append(buffer, 0, rsz);
+        }
+        return out.toString();
+    }
+
 }
