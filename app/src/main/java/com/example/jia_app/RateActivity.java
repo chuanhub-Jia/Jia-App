3ded224b2e1a804336020cf8657db24a64989e1d
chuanJia <jiashiying971@outlook.com>
2020-06-05 20:02:54 +0800

    rate app

---
+++ app/src/main/java/com/example/jia_app/RateActivity.java
@@ -0,0 +1,50 @@
+package com.example.jia_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class RateActivity extends AppCompatActivity {
+    private final String TAG = "Rate";
+    private float dollarRate = 0.142f;
+    private float euroRate = 0.125f;
+    private float wonRate = 0.3f;
+    EditText rmb;
+    TextView show;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate);
+        setContentView(R.layout.activity_rate);
+        rmb = (EditText) findViewById(R.id.rmb);
+        show = (TextView) findViewById(R.id.show);
+    }
+    public void onClick(View btn){
+        Log.i(TAG, "onClick: ");
+        String str = rmb.getText().toString();
+        Log.i(TAG, "onClick: get str=" + str);
+
+        float r = 0;
+        if(str.length()>0){
+            r = Float.parseFloat(str);
+        }else{
+            //用户没有输入内容
+            Toast.makeText(this, "请输入内容", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Log.i(TAG, "onClick: r=" + r);
+        if(btn.getId()==R.id.dollar){
+            show.setText(String.valueOf(r*dollarRate));
+        }else if(btn.getId()==R.id.euro){
+            show.setText(String.valueOf(r*euroRate));
+        }else{
+            show.setText(String.valueOf(r*wonRate));
+        }
+    }
+}
